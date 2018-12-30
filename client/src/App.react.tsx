d64b1e0c6a4f5776876424c141dfc0630468bb99
Eric Mai <ericmak624@gmail.com>
2018-12-30 03:08:26 -0800

    init

--- client/src/App.react.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-function App() {
-  return <div>Hello</div>;
-}
-
-ReactDOM.render(<App />, document.getElementById('content'));
